656c96212af50adb1a43a396876a4dee13414d48
George <ustinovgo@gmail.com>
2021-09-25 17:14:18 +0300

    started game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 GREEN = '\033[32m'
 YELLOW = '\033[33m'
 WHITE = '\033[37m'
@@ -20,10 +22,20 @@ def greetings():
     print(GREEN + 'Приветствую вас в консольной версии игры Сапёр!')
 
 
-def actions_before_start():
+def input_start():
     print(GREEN + 'Если хотите начать новую игру - введите \'new\'')
     print('Если хотите загрузить игру - введите \'load <path>\', где path - путь до игры на вашем компьютере')
     print('Если хотите выйти из игры - введите \'exit\'')
+
+
+def input_dimensions():
+    print(GREEN +
+          'Введите параметры поля в формате \'n m k\', где n - число строк, m - число столбцов, k - число мин')
+
+
+def input_move():
+    print(GREEN + '\'Open X Y\' - открыть клетку с координатами (X, Y)')
+    print('\'Flag X Y\' - поставить флажок в клетку с координатами (X, Y)')
 
 
 def error_input():
@@ -42,36 +54,60 @@ def end():
     print(GREEN + 'Спасибо за игру!')
 
 
+def can_open(field):
+    for i in range(len(field)):
+        for j in range(len(field[i])):
+            if field[i][j].field == '#':
+                return True
+    return False
+
+
+def print_all_field(field):
+    for i in range(len(field)):
+        for j in range(len(field[i])):
+            field[i][j].print_field()
+        print()
+
+
 def main():
     greetings()
     while True:
-        actions_before_start()
+        input_start()
 
         command = input().split(' ')
-        if command[0].lower() == 'new':
+        if command[0].lower() == 'new' and len(command) == 1:
             flag = True
             while flag:
                 flag = False
-                print(GREEN +
-                      'Введите параметры поля в формате \'n m k\', где n - число строк, m - число столбцов, k - число мин')
+                input_dimensions()
                 parameters = input().split()
                 if len(parameters) != 3 or not parameters[0].isnumeric() or not parameters[1].isnumeric() or not \
                         parameters[2].isnumeric():
                     flag = True
                     error_input()
                     continue
-                n, m, k = map(int, parameters)
-                if n == 0 or m == 0 or k == 0:
+                n, m, mines = map(int, parameters)
+                if n == 0 or m == 0:
                     flag = True
                     error_dimension()
                     continue
-                elif k > n * m:
+                elif mines > n * m:
                     flag = True
                     error_extra_mines()
                     continue
-        elif command[0].lower() == 'load':
+            player_field = [[Cell()] * n for i in range(m)]
+            flags_left = mines
+            while can_open(player_field):
+                print_all_field(player_field)
+                input_move()
+                command = input().split()
+
+
+
+
+        elif command[0].lower() == 'load' and len(command) == 2:
             pass
-        elif command[0].lower() == 'exit':
+        elif command[0].lower() == 'exit' and len(command) == 1:
             end()
             break
         else:
